c3b8b2f9269d86c72e11757f8c70e88b0da53c91
TabithaMaud <tabitha.m.perry@gmail.com>
2020-09-04 13:03:17 -0400

    Add useState and useEffect, change App.js to functional component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route, Link, Redirect } from 'react-router-dom';
-import React, { Component } from 'react';
 import Main from './components/Main/Main';
 import CreateIncident from './components/CreateIncident/CreateIncident';
 import FooterNav from './components/FooterNav/FooterNav';
@@ -10,32 +9,27 @@ import OfficerDetail from './components/OfficerView/OfficeDetail';
 import Profile from './components/Profile/Profile';
 import EditIncident from './components/Profile/EditIncident';
 import IncidentDetail from './components/RecentIncidentView/IncidentDetail';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			incidents: [],
-			officers: [],
-		};
-	}
-	render() {
-		return (
-			<div>
-				<main>
-					<HeaderNav />
-					<Route path='/' exact component={Main} />
-					<Route path='/incidents/new' component={CreateIncident} />
-					<Route path='/officers' component={OfficerView} />
-					<Route path='/officers/:id' component={OfficerDetail} />
-					<Route path='/profile' component={Profile} />
-					<Route path='/incidents/:id/edit' component={EditIncident} />
-					<Route path='/incidents/:id' component={IncidentDetail} />
-					<FooterNav />
-				</main>
-			</div>
-		);
-	}
+function App() {
+	const [incidents, setIncidents] = useState([]);
+	const [officers, setOfficers] = useState([]);
+
+	return (
+		<div>
+			<main>
+				<HeaderNav />
+				<Route path='/' exact component={Main} />
+				<Route path='/incidents/new' component={CreateIncident} />
+				<Route path='/officers' component={OfficerView} />
+				<Route path='/officers/:id' component={OfficerDetail} />
+				<Route path='/profile' component={Profile} />
+				<Route path='/incidents/:id/edit' component={EditIncident} />
+				<Route path='/incidents/:id' component={IncidentDetail} />
+				<FooterNav />
+			</main>
+		</div>
+	);
 }
 
 export default App;

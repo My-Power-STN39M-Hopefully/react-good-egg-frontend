c3633d14332f09c2ff041e6bb4d45c29a83a2565
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-07 09:06:22 -0500

    started footer before checking out of the branch

--- src/components/FooterNav/FooterNav.js
+++ src/components/FooterNav/FooterNav.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Route, Link, Redirect } from 'react-router-dom';
+import './FooterNav.css';
 
 function FooterNav() {
-	return <div>Foot Nav</div>;
+	return (
+		<div className='footer-nav'>
+			<a href='/incidents'>incidents</a>
+			<a href='/incidents/new'>[+]</a>
+			<a href='/profile'>profile</a>
+		</div>
+	);
 }
 
 export default FooterNav;

0cc3914b58ab77addb1d985adea4c2ae419a14ed
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-08 17:38:12 -0500

    fleshed out base forms for signin and signup

--- src/components/CreateIncident/CreateIncident.js
+++ src/components/CreateIncident/CreateIncident.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Link, Redirect } from 'react-router-dom';
 
-function CreateIncident() {
+function CreateIncident(props) {
+	useEffect(() => {
+		props.usersHandler();
+	});
 	return <div>Create Incident</div>;
 }
 

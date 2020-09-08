9197bc1327bc26f5de681e0678513accbda6391d
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-08 14:12:56 -0500

    working on hamburger

--- src/components/HeaderNav/Sidebar/SideBar.js
+++ src/components/HeaderNav/Sidebar/SideBar.js
@@ -4,15 +4,22 @@ import './SideBar.css';
 
 function SideBar() {
 	return (
-		<Menu>
-			<a href='/incidents/new'>New Incident</a>
-			<a href='/profile'>Profile</a>
-			<a href='/about'>About</a>
-			{/* <a href='/search'>Search</a> */}
-			<div>
-				<button className='logout-button'>Logout</button>
-			</div>
-		</Menu>
+		<div id='outer-container'>
+			<Menu
+				pageWarpId={'page-wrap'}
+				outerContainerId={'outer-container'}
+				width={'100%'}>
+				<main id='page-wrap'>
+					<a href='/incidents/new'>New Incident</a>
+					<a href='/profile'>Profile</a>
+					<a href='/about'>About</a>
+					{/* <a href='/search'>Search</a> */}
+					<div>
+						<button className='logout-button'>Logout</button>
+					</div>
+				</main>
+			</Menu>
+		</div>
 	);
 }
 

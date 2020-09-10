c197b4556f99cf377bea14d51cb6d37ede9de093
TabithaMaud <tabitha.m.perry@gmail.com>
2020-09-10 12:44:53 -0400

    replace 'ReactRouter' with 'HashRouter'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(

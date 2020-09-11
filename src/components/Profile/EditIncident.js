4b13d6878c5d263f94b6f9a5d1d3f0ec046af242
TabithaMaud <tabitha.m.perry@gmail.com>
2020-09-11 13:09:35 -0400

    finished most of the logic for edit and delete incident with pseudo code for next steps when db is live

--- src/components/Profile/EditIncident.js
+++ src/components/Profile/EditIncident.js
@@ -17,26 +17,43 @@ class EditIncident extends Component {
 	};
 
 	handleInputChange = (event) => {
+		const editIncident = this.state.editIncident;
 		this.setEditIncident({
-			category: this.editedIncident.category,
-			officers: this.editedIncident.officers,
-			officer_description: this.editedIncident.officer_description,
-			description: this.editedIncident.description,
-			date: this.editedIncident.date,
-			time: this.editedIncident.time,
-			location: this.editedIncident.location,
-			formal_complaint: this.editedIncident.formal_complaint,
-			formal_complaint_number: this.editedIncident.formal_complaint_number,
-			witnesses_present: this.editedIncident.witnesses_present,
-			witnesses_information: this.editedIncident.witnesses_information,
-			private: this.editedIncident.private,
+			category: editIncident.category,
+			officers: editIncident.officers,
+			officer_description: editIncident.officer_description,
+			description: editIncident.description,
+			date: editIncident.date,
+			time: editIncident.time,
+			location: editIncident.location,
+			formal_complaint: editIncident.formal_complaint,
+			formal_complaint_number: editIncident.formal_complaint_number,
+			witnesses_present: editIncident.witnesses_present,
+			witnesses_information: editIncident.witnesses_information,
+			private: editIncident.private,
 			...{ [event.target.id]: event.target.value },
 		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		const newIncident = this.state.editedIncident;
+		const url = `/incidents/edit/${newIncident.id}`;
+		// newIncident will be the updated object we sent in the PUT request.
+		//url endpoint may have to be adjusted to match backend.
+		//we can run put request and then redirect to profile.
+	};
+
+	deleteIncident = (event) => {
+		event.preventDefault();
+		const newIncident = this.state.editedIncident;
+		const url = `/incidents/${newIncident.id}`;
 	};
 
 	render() {
 		let result = {};
 		// will be doing a fetch below to retrieve individual incident, waiting for database to go live
+		// this will likely load on didmount to update editedIncident in state
 		for (let i = 0; i < this.props.incidents.length; i++) {
 			if (this.props.editIncidentId === this.props.incidents[i].id.toString()) {
 				result = this.props.incidents[i];
@@ -46,7 +63,7 @@ class EditIncident extends Component {
 		return (
 			<div className='create-form'>
 				<h1>Edit Incident Form</h1>
-				<Form className='incidentForm'>
+				<Form className='incidentForm' onSubmit={this.handleSubmit}>
 					<Form.Group controlId='category'>
 						<Form.Label>Category</Form.Label>
 						<Form.Control
@@ -196,8 +213,10 @@ class EditIncident extends Component {
 							onChange={this.handleInputChange}
 						/>
 					</Form.Group>
-					<button>Submit</button>
-					<button className='deleteButton'>Delete</button>
+					<button type='submit'>Submit</button>
+					<button className='deleteButton' onClick={this.deleteIncident}>
+						Delete
+					</button>
 				</Form>
 			</div>
 		);

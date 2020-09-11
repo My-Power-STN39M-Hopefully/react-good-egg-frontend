704551e668441993b4c1ba7148a110ddce9b9dd9
TabithaMaud <tabitha.m.perry@gmail.com>
2020-09-11 14:02:06 -0400

    refine styling on incident-card using grid

--- src/components/RecentIncidentView/RecentIncidentView.js
+++ src/components/RecentIncidentView/RecentIncidentView.js
@@ -4,20 +4,19 @@ import './RecentIncidentView.css';
 
 function RecentIncidentView(props) {
 	return (
-		<div className='incidentSmall'>
-			<Link to={'incidents/' + props.id}>
+		<Link to={'incidents/' + props.id}>
+			<div className='incidentSmall'>
 				{props.bad_apple === true ? (
 					<div className='badApple'>Bad Apple</div>
 				) : (
 					<div className='goodEgg'>Good Egg</div>
 				)}
-				<h3>
-					{props.category} : {props.date}
-				</h3>
+				<h3>{props.category}</h3>
+				<p className='date'> {props.date}</p>
 				<p>{props.description}</p>
 				<p>Officer/s Involved: {props.officers}</p>
-			</Link>
-		</div>
+			</div>
+		</Link>
 	);
 }
 

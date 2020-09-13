b1c28e480aad4b766f653322b7230b32dbf00b0a
TabithaMaud <tabitha.m.perry@gmail.com>
2020-09-13 18:42:01 -0400

    connect officer view and detail to api, style officer detail

--- src/components/OfficerView/OfficerView.js
+++ src/components/OfficerView/OfficerView.js
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { Route, Link, Redirect } from 'react-router-dom';
 import officerData from '../../officerData.json';
 import './OfficerView.css';
+import { GoodEggBackend } from '../../api/GoodEggBackend';
 
-function OfficerView(props) {
-	useEffect(() => {
-		const url =
-			'http://ec2-18-224-153-210.us-east-2.compute.amazonaws.com:8000/officers/apples-and-eggs';
-		fetch(url)
-			.then((response) => response.json())
-			.then((response) => {
-				props.officersHandler(response.bad_apples.concat(response.good_eggs));
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-
+class OfficerView extends Component {
+	componentDidMount() {
 		window.scrollTo(0, 0);
-	});
+		GoodEggBackend()
+			.get('officer/')
+			.then((response) => {
+				this.props.officersHandler(response.data);
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.log('error');
+			});
+	}
 
-	return (
-		<div className='officerList'>
-			{props.officers.map((officer) => {
-				return (
-					<div className='officerSmall'>
-						<Link to={'officers/' + officer.id}>
-							<h3>
-								{officer.first_name} {officer.last_name}
-							</h3>
-							<p>DOB: {officer.dob}</p>
-							<p>
-								{officer.race} {officer.gender}
-							</p>
-							{/* will need to figure out logic for pulling corresponding incidents for each officer */}
-							<p>Bad Apple Incidents: 100</p>
-						</Link>
-					</div>
-				);
-			})}
-		</div>
-	);
+	render() {
+		return (
+			<div className='officerList'>
+				{this.props.officers.map((officer) => {
+					return (
+						<div className='officerSmall'>
+							<Link to={'officers/' + officer.id}>
+								<h3>
+									{officer.first_name} {officer.last_name}
+								</h3>
+								<p>DOB: {officer.dob}</p>
+								<p>
+									{officer.race} {officer.gender}
+								</p>
+								{/* will need to figure out logic for pulling corresponding incidents for each officer */}
+								<p>Bad Apple Incidents: 100</p>
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
-
 export default OfficerView;

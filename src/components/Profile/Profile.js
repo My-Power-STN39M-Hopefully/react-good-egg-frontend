4143973a58437358a906728ff46ae32bacd2b9fa
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-14 13:40:52 -0500

    fixed button issues on goodeggs badapples

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -50,6 +50,7 @@ function Profile(props) {
 	const handleEditClick = (event) => {
 		props.editIncidentHandler(event.target.id);
 	};
+	console.log(userData);
 	return (
 		<div className='profile'>
 			<div className='user-icon'>{String(userData.first_name).charAt(0)}</div>
